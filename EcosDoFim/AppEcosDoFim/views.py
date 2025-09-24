395a3679fcc35121ceae38c5ff0427d343ab82b2
joseboadeiro <brenogsantiago@gmail.com>
2025-09-24 19:10:29 -0300

    def Escolha de dificuldade
    provavelmente a gente vai ter que voltar aqui pra corrigir alguma coisa :/

--- EcosDoFim/AppEcosDoFim/views.py
+++ EcosDoFim/AppEcosDoFim/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+def escolher_dificuldade(request):
+    niveis = [1, 2, 3, 4]  
+
+    if request.method == "POST":
+        dificuldade_escolhida = request.POST.get("dificuldade")
+        print("Dificuldade escolhida:", dificuldade_escolhida)#aqui e so pra ligar co o html
+        
+
+    return render(request, "escolher_dificuldade.html", {"niveis": niveis})
+
+
+ #considerações pra maju: aqui esse def ele funciona pra gente seleciona a dificuldade do jogo
+ #o objetivo aqui é que la na frente quandoa a gente for trabalhar com o Dcbs ja tenha a dificuldade do jogo estabelecida
