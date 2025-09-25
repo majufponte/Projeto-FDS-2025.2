084bfec00b3427b35881e819ee9af05d1b880877
Davilapeixoto <davilapeixoto52@gmail.com>
2025-09-25 16:41:06 -0300

    Parte de dificuldade já esta funcionando
    Agora é testar com o audio

--- EcosDoFim/AppEcosDoFim/views.py
+++ EcosDoFim/AppEcosDoFim/views.py
@@ -21,7 +21,7 @@ def escolher_dificuldade(request):
         request.session["limiar_dificuldade"] = limiar
 
 
-    return render(request, "escolher_dificuldade.html")
+    return render(request, "escolher_dificuldade.html", {"niveis": niveis})
 
  #considerações pra maju: aqui esse def ele funciona pra gente seleciona a dificuldade do jogo
  #o objetivo aqui é que la na frente quandoa a gente for trabalhar com o Dcbs ja tenha a dificuldade do jogo estabelecida
