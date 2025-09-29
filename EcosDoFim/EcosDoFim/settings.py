3deb6fc23e4659af8cd8dbd85f4b3963110b2535
Davilapeixoto <davilapeixoto52@gmail.com>
2025-09-29 18:34:33 -0300

    imagem funcional novamente

--- EcosDoFim/EcosDoFim/settings.py
+++ EcosDoFim/EcosDoFim/settings.py
@@ -149,8 +149,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-#STATIC_URL = "/static/"
-STATIC_URL = os.environ.get('DJANGO_STATIC_URL', "/static/")
+STATIC_URL = "/static/"
+#STATIC_URL = os.environ.get('DJANGO_STATIC_URL', "/static/")
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     
 STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
